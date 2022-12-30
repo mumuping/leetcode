c605c5028c11aeef2a24a9b235648932b6d99117
wang_linping <1262936318@qq.com>
2022-12-30 17:49:02 +0800

    feat: add the topological sorting solution

--- graph/210_course_schedule_II.cpp
+++ graph/210_course_schedule_II.cpp
@@ -3,7 +3,9 @@
 //
 #include "vector"
 #include "queue"
+#include "algorithm"
 
+// 方法一：入度
 std::vector<int> findOrder(int numCourses, std::vector<std::vector<int>> &prerequisites) {
   std::vector<std::vector<int>> graph(numCourses);
   std::vector<int> degree(numCourses), res;
@@ -62,4 +64,37 @@ std::vector<int> findOrder_(int numCourses, std::vector<std::vector<int>> &prere
 
   if (res.size()!=numCourses) return {};
   return res;
+}
+
+// 方法二：拓扑排序，后序遍历的逆序
+bool dfs(const std::vector<std::vector<int>> &graph, std::vector<bool> &path_for_cycle,
+		 std::vector<bool> &is_visited, std::vector<int> &orders, int node) {
+  if (path_for_cycle[node]) return true;
+  if (is_visited[node]) return false;
+  is_visited[node] = path_for_cycle[node] = true;
+  for (int next : graph[node]) {
+	if (dfs(graph, path_for_cycle, is_visited, orders, next)) return true;
+  }
+  orders.push_back(node);
+  path_for_cycle[node] = false;
+  return false;
+}
+
+std::vector<int> findOrder_topological_sort(int numCourses, std::vector<std::vector<int>> &prerequisites) {
+  std::vector<std::vector<int>> graph(numCourses, std::vector<int>());
+  for (const auto it : prerequisites) {
+	graph[it[1]].push_back(it[0]);
+  }
+
+  std::vector<bool> path_for_cycle(numCourses, false);
+  std::vector<bool> is_visited(numCourses, false);
+  std::vector<int> orders;
+  for (int i = 0; i < numCourses; ++i) {
+	if (!is_visited[i]) {
+	  if (dfs(graph, path_for_cycle, is_visited, orders, i)) return {};
+	}
+  }
+
+  std::reverse(orders.begin(), orders.end());
+  return orders;
 }
