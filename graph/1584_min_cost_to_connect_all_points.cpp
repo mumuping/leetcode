65cde579dc0b44a21cc7a5c1c2da5893b751cd84
mumuping <1262936318@qq.com>
2023-01-13 21:02:34 +0800

    feat: add the prim algorithm of No.1584

--- graph/1584_min_cost_to_connect_all_points.cpp
+++ graph/1584_min_cost_to_connect_all_points.cpp
@@ -1,5 +1,6 @@
 #include "algorithm"
 #include "numeric"
+#include "queue"
 #include "tuple"
 #include "vector"
 
@@ -49,4 +50,66 @@ int minCostConnectPoints(std::vector<std::vector<int>>& points) {
   }
 
   return cost;
+}
+
+// prim
+class Comp {
+ private:
+  typedef std::pair<int, int> t;
+
+ public:
+  bool operator()(const t& lhs, const t& rhs) const {
+    return rhs.second < lhs.second;
+  }
+};
+
+inline void get_edges(
+    const std::vector<std::vector<std::pair<int, int>>>& graph,
+    const std::vector<bool>& in_mst,
+    std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>,
+                        Comp>& pq,
+    int node) {
+  for (const auto& edge : graph[node]) {
+    if (!in_mst[edge.first]) {
+      pq.push(edge);
+    }
+  }
+}
+
+int minCostConnectPoints_prim(std::vector<std::vector<int>>& points) {
+  int n = points.size();
+  if (n <= 1) return 0;
+
+  std::vector<std::vector<std::pair<int, int>>> graph(
+      n, std::vector<std::pair<int, int>>());
+  int distance;
+  for (int i = 0; i < n; ++i) {
+    for (int j = i + 1; j < n; ++j) {
+      distance = std::abs(points[i][0] - points[j][0]) +
+                 std::abs(points[i][1] - points[j][1]);
+      graph[i].push_back(std::make_pair(j, distance));
+      graph[j].push_back(std::make_pair(i, distance));
+    }
+  }
+
+  std::priority_queue<std::pair<int, int>, std::vector<std::pair<int, int>>,
+                      Comp>
+      pq;
+  std::vector<bool> in_mst(n, false);
+  in_mst[0] = true;
+  get_edges(graph, in_mst, pq, 0);
+
+  int to, weight, weight_sum = 0;
+  while (!pq.empty()) {
+    to = pq.top().first;
+    weight = pq.top().second;
+    pq.pop();
+    if (!in_mst[to]) {
+      weight_sum += weight;
+      in_mst[to] = true;
+      get_edges(graph, in_mst, pq, to);
+    }
+  }
+
+  return weight_sum;
 }
